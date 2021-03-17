61e3f0e37a47aa968530236f6c747d49e834dab8
Kang-kyunghun <korea271150@gmail.com>
2021-03-17 19:53:05 +0900

    list view 통신 확인

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name        = models.CharField(max_length=100)
+    image_url   = models.URLField(max_length=2000)
+    description = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table    = 'products'
--- product/views.py
+++ product/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+from .models                import Product
+
+class ProductsView(View):   
+    def get(self, request):    
+        offset = int(request.GET.get("offset",0))
+        limit  = int(request.GET.get("limit",1))
+        
+        products = Product.objects.all()[offset:offset+limit]
+        data = {
+            "count"  : products.count(),
+            "result" : [{
+                            "id"          : product.id,
+                            "name"        : product.name,
+                            "image_url"   : product.image_url,
+                            "description" : product.description
+                        } for product in products]
+        }
+        
+        return JsonResponse({'products': data}, status=200)
